e541d82ea196aaa810d5c48a15cf1c4fcdc98769
chantmsss <49mgntic@gmail.com>
2020-12-06 17:26:38 +0900

    add WriteDramaActivity

---
+++ Nadri2020/app/src/main/java/com/example/nadri2020/WriteDramaActivity.java
@@ -0,0 +1,14 @@
+package com.example.nadri2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteDramaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_drama);
+    }
+}
