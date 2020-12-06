8249ee139186c465b8a6ce0ef149a42fc5721c43
chantmsss <49mgntic@gmail.com>
2020-12-06 17:23:23 +0900

    add SearchDramaActivity

---
+++ Nadri2020/app/src/main/java/com/example/nadri2020/SearchDramaActivity.java
@@ -0,0 +1,14 @@
+package com.example.nadri2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchDramaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_drama);
+    }
+}
