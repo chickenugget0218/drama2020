0db86af9109e45fca3884685cc362c90ff415782
chickenugget0218 <chickenugget0218@gmail.com>
2021-01-12 23:43:26 +0900

    20210112

--- Nadri2020/app/src/main/java/com/example/nadri2020/OnMainRecordClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.nadri2020;
-
-import android.view.View;
-
-public interface OnMainRecordClickListener {
-
-    public void onItemClick(MainAdapter.MainViewHolder holder, View view, int position);
-}
