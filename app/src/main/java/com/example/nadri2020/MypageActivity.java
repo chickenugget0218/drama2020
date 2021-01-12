4d3acb5e3fe68fef0ade32eaa7c258ee8ba319b6
chickenugget0218 <chickenugget0218@gmail.com>
2021-01-12 23:14:47 +0900

    20210112

---
+++ app/src/main/java/com/example/nadri2020/MypageActivity.java
@@ -0,0 +1,63 @@
+package com.example.nadri2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class MypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+        moveToMain();
+        moveToBoard();
+    }
+
+    public void moveToMain(){
+        RelativeLayout button = findViewById(R.id.main_act_home_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_out_right, R.anim.slide_in_left);
+                finish();
+            }
+        });
+    }
+
+    public void moveToBoard(){
+        RelativeLayout button = findViewById(R.id.main_act_board_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BoardActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_out_right, R.anim.slide_in_left);
+                finish();
+            }
+        });
+    }
+
+
+    // 뒤로 가기 버튼 두 번 눌러야 종료되게
+    private long time = 0;
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+
+        if (System.currentTimeMillis() >= 2000 + time) {
+            time = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(), "뒤로 가기 버튼을 한 번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show();
+        } else if (System.currentTimeMillis() < 2000 + time) {
+            finish();
+        }
+    }
+
+}
